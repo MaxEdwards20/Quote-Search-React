1acbbe8fade5d4712a69b01b15d48ecdb5f9895c
medwards <maxedwards20@gmail.com>
2023-01-15 16:14:03 -0700

    Well developed look and the search param can be passed around correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { FormInput } from "./components/FormInput";
@@ -10,61 +10,67 @@ interface Quote {
 }
 
 function App() {
-  const [quote, setQuote] = useState();
-  const [quoteBox, addQuotes] = useState();
-  const [searchParam, changeParam] = useState();
+  const [quotes, setQuotes] = useState([]);
+  const [searchParam, setSearchParam] = useState("");
+
+  function handleSearchClick() {
+    async function fetchData() {
+      let url = "https://api.quotable.io/search/quotes?query=" + searchParam;
+      const result = await fetch(url);
+      let json = await result.json();
+      setQuotes(json.results);
+    }
+    if (searchParam && searchParam.length > 0) {
+      fetchData();
+    }
+  }
+
+  async function getRandomQuote() {
+    const result = await fetch("https://api.quotable.io/random");
+    return await result.json();
+  }
+
+  function handleSearchInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchParam(event.target.value);
+  }
+
   return (
-    <div className="App" color="blue">
-      <div> {quote} </div>
-      <div className="card">
-        <button
-          onClick={async () => {
-            let quote = await getRandomQuote();
-            let formatted = formatQuote(quote);
-            setQuote(formatted); // What can I use here? How do I adjust the types for these state objects?
-          }}
-        >
-          Click for a random Quote
-        </button>
+    <div className="card bg-primary" style={{ width: "100%" }}>
+      <div className="card-body">
+        <h1 className="card-title text-white"> Find a Quote </h1>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            value={searchParam}
+            onChange={handleSearchInput}
+            className="form-control"
+            placeholder="Search Quotes Here"
+          ></input>
+          <div className="input-group-prepend">
+            <button
+              type="button"
+              className="btn btn-secondary btn-large btn-block mr-2 padding-3"
+              onClick={handleSearchClick}
+            >
+              Search
+            </button>
+          </div>
+          <div>
+            {
+              <ul className="list-group">
+                {quotes.map((quote: Quote) => (
+                  <QuoteHolder
+                    author={quote.author}
+                    content={quote.content}
+                  ></QuoteHolder>
+                ))}
+              </ul>
+            }
+          </div>
+        </div>
       </div>
-      <div className="card">
-        <FormInput label="Search"> {searchParam}</FormInput>{" "}
-        {/* I am not sure why this FormInput is red underlined */}
-        <button
-          onClick={async () => {
-            let quotes = await searchQuotes({ searchParam }); // How can I send the value from the form to the function to search with?
-            console.log(quotes.results);
-            let quoteHolders = quotes.map((quote: Quote) => (
-              <QuoteHolder
-                author={quote.author}
-                content={quote.content}
-              ></QuoteHolder>
-            ));
-            addQuotes(quoteHolders); // why does this not work and display these QuoteHolder elements?
-          }}
-        >
-          Search
-        </button>
-      </div>
-      <div>{quoteBox}</div>
     </div>
   );
-}
-
-async function getRandomQuote() {
-  const result = await fetch("https://api.quotable.io/random");
-  return await result.json();
-}
-
-async function searchQuotes(quote: string) {
-  // How can I pass the value from the FormInput down here? Is using a state value the best way?
-  let url = "https://api.quotable.io/search/quotes?query=" + quote;
-  const result = await fetch(url);
-  return await result.json();
-}
-
-function formatQuote(quote: Quote) {
-  return quote.content + " --- " + quote.author;
 }
 
 export default App;
--- src/components/QuoteHolder.tsx
+++ src/components/QuoteHolder.tsx
@@ -1,13 +1,14 @@
 interface Quote {
   author: string;
   content: string; // ? means this is optional
+  children?: [];
 }
 
 export function QuoteHolder({ author, content }: Quote) {
   return (
     <div className="card">
-      {content + " --- "}
-      {author}
+      {content}
+      <div className="card-body ">{author}</div>
     </div>
   );
 }
